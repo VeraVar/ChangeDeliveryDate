abc05daae80bceec1bc5cf6504ad0c9dfc21df8d
Vera Var <primavera1919@yandex.ru>
2020-09-01 14:31:08 +0300

    Notification text corrected

--- src/test/java/ru/netology/ChangeDeliveryDateTest.java
+++ src/test/java/ru/netology/ChangeDeliveryDateTest.java
@@ -23,13 +23,13 @@ public class ChangeDeliveryDateTest {
         $("[data-test-id='agreement'] .checkbox__box").click();
         $(".button").click();
         $("[data-test-id='success-notification']").waitUntil(visible, 15000).shouldHave(text("Успешно! " +
-                "Встреча успешно забронирована на " + date));
+                "Встреча успешно запланирована на " + date));
         $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id='date'] input").sendKeys(date);
         $(".button").click();
-        $("[data-test-id='replan-notification']").shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+        $("[data-test-id='replan-notification']").shouldHave(text("Необходимо подтверждение " + "У вас уже запланирована встреча на другую дату. Перепланировать?"));
         $(".notification_visible .button").click();
         $("[data-test-id='success-notification']").waitUntil(visible, 15000).shouldHave(text("Успешно! " +
-                "Встреча успешно забронирована на " + date));
+                "Встреча успешно запланирована на " + date));
     }
 }
